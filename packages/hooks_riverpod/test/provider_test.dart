8a976868bed1912c94c50e8c165f7a709292d051
Remi Rousselet <darky12s@gmail.com>
2021-08-21 14:50:03 +0100

    upgrade flutter_hooks and fix lints

--- packages/hooks_riverpod/test/provider_test.dart
+++ packages/hooks_riverpod/test/provider_test.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:mockito/mockito.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
   testWidgets('.read(context)', (tester) async {
